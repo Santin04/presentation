af8e59c09a6fae823a7f8e008d081b8f224993e1
Santin04 <masantinte@gmail.com>
2024-04-15 18:33:12 -0300

    carroussel images and texts

---
+++ script.js
@@ -0,0 +1,49 @@
+//selecionando os elementos html que vão ser alterados
+const content_text_top = document.querySelector("#content_text_top");
+const image_graphic_center = document.querySelector("#image_graphic_center");
+const content_text_button = document.querySelector("#content_text_button");
+const image_graphic_buttom = document.querySelector("#image_graphic_buttom");
+
+//criando os 
+const texts_top = [
+    'Projeto 01',
+    'Projeto 02',
+    'Projeto 03',
+    'Projeto 04',
+];
+
+const texts_buttom = [
+    'Texto do projeto 01',
+    'Texto do projeto 02',
+    'Texto do projeto 03',
+    'Texto do projeto 04',
+];
+
+const images_center = [
+    'images/teste01.png',
+    '',
+    'images/teste01.png',
+    '',
+];
+
+const images_buttom = [
+    'images/teste02.png',
+    '',
+    'images/teste02.png',
+    '',
+];
+
+let indice = 0;
+
+function edit_items() {
+    content_text_top.innerText = texts_top[indice];
+    content_text_button.innerText = texts_buttom[indice];
+    image_graphic_center.src = images_center[indice];
+    image_graphic_buttom.src = images_buttom[indice];
+
+    indice = (indice + 1) % texts_top.length;
+}
+
+edit_items();
+
+setInterval(edit_items, 2000);
